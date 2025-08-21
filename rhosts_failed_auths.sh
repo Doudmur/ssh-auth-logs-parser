5bab185082c9a0737d03c9f1942a57ad234348fb
Doudmur <bbeka544@mail.ru>
2025-08-21 11:11:18 +0500

    Get IPs since last scan and save results

--- rhosts_failed_auths.sh
+++ rhosts_failed_auths.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-# Script to get failed SSH rhosts from /var/log/auth.log
+STATE_FILE="$HOME/.failed_rhosts.state"
+OUTPUT_FILE="$HOME/failed_rhosts.log"
 
-awk -F'rhost=' '/authentication failure/ {
-    split($2,a," ");
-    print a[1]
-}' /var/log/auth.log | wc -l
+# If no state file, this is first run
+if [[ ! -f "$STATE_FILE" ]]; then
+    since="5 minutes ago"
+else
+    since=$(cat "$STATE_FILE")
+fi
+
+# Current time (will be saved as new state)
+now=$(date --iso-8601=seconds)
+
+# Get logs from "since" until "now"
+journalctl -u ssh --since "$since" --until "$now" \
+  | awk -F'rhost=' '/authentication failure/ {
+        split($2,a," ");
+        print a[1]
+    }' >> "$OUTPUT_FILE"
+
+# Save the last timestamp for next run
+echo "$now" > "$STATE_FILE"
+
